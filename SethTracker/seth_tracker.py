3ec66b82045e8682e4422b874f9a498166dddd46
Pete Alexander <peter.d.alexander23@gmail.com>
2020-07-13 13:14:05 +0100

    Add base main window to seth_tracker
    Add basic main window widget to seth_tracker for testing Stat Tracker
    compound widget. Global application style has been set to "Fusion" for
    now.

---
+++ SethTracker/seth_tracker.py
@@ -0,0 +1,27 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
+
+import sys
+
+import gui_elements
+
+class SethTracker(QtWidgets.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(SethTracker, self).__init__(*args, **kwargs)
+
+        self.setWindowTitle("Seth Tracker")
+
+        testTracker = gui_elements.StatTracker("Test:", 5, 10)
+
+        self.setCentralWidget(testTracker)
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+
+    app.setStyle('Fusion')
+
+    window = SethTracker()
+    window.show()
+
+    app.exec_()
