fc6a264a90267966fa487cb851074703afab9023
Pete Alexander <peter.d.alexander23@gmail.com>
2020-07-14 11:10:17 +0100

    Add basic Gui structure
    Create a central layout in seth_tracker and place StatTrackers with
    the chosen attributes into it. The Trackers are first organised by
    category then placed into QGroupBoxes for visual differentiation.
    These group boxes are added to the central layout within a QVBox.
    
    In its current form, the app can be used with the values hardcoded into
    the StatTrackers but further work is required for saving the values and
    loading them back into the app on start up.

--- SethTracker/seth_tracker.py
+++ SethTracker/seth_tracker.py
@@ -1,18 +1,56 @@
 from PyQt5 import QtWidgets
 import sys
 
-import gui_elements
-
+from gui_elements import StatTracker as tracker
 
 class SethTracker(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(SethTracker, self).__init__(*args, **kwargs)
 
         self.setWindowTitle("Seth Tracker")
+        
+        # Construct all the stat trackers
+        hp_track = tracker("HP:", 22, 22)
+        temp_hp_track = tracker("Temp HP:", 0, 999)
+        hit_dice_track = tracker("Hit Dice", 2, 4)
+        lv1_spell_track = tracker("Lv.1 Spell Slots:", 1, 4)
+        lv2_spell_track = tracker("Lv.2 Spell Slots:", 1, 3)
+        bard_insp_track = tracker("Bardic Inspiration:", 2, 3)
 
-        testTracker = gui_elements.StatTracker("Test:", 5, 10)
+        # Group trackers into catagories
+        health_layout = QtWidgets.QVBoxLayout()
+        health_layout.addWidget(hp_track)
+        health_layout.addWidget(temp_hp_track)
+        health_layout.addWidget(hit_dice_track)
 
-        self.setCentralWidget(testTracker)
+        spells_layout = QtWidgets.QVBoxLayout()
+        spells_layout.addWidget(lv1_spell_track)
+        spells_layout.addWidget(lv2_spell_track)
+
+        bardic_layout = QtWidgets.QVBoxLayout()
+        bardic_layout.addWidget(bard_insp_track)
+
+        # Create set of group boxes
+        health_group = QtWidgets.QGroupBox()
+        health_group.setTitle("Health")
+        health_group.setLayout(health_layout)
+
+        spells_group = QtWidgets.QGroupBox()
+        spells_group.setTitle("Spell Slots")
+        spells_group.setLayout(spells_layout)
+
+        bardic_group = QtWidgets.QGroupBox()
+        bardic_group.setTitle("Bardic Abilities")
+        bardic_group.setLayout(bardic_layout)
+
+        # Create central layout to house all sublayouts
+        self._central_widget = QtWidgets.QWidget()
+        self.setCentralWidget(self._central_widget)
+
+        top_layout = QtWidgets.QVBoxLayout(self._central_widget)
+        top_layout.addWidget(health_group)
+        top_layout.addWidget(spells_group)
+        top_layout.addWidget(bardic_group)
 
 
 if __name__ == "__main__":
